35baed9ed2437f87e8e72a5e0e6a757052e238bd
theisaackim <isaakim91@gmail.com>
2022-08-25 21:02:32 -0700

    add twirls

--- main.js
+++ main.js
@@ -59,7 +59,6 @@ instagram.onmouseleave = () => {
     instagram.style.color = '#f7f3ee';
     instagram.style.border = '';
 }
-
 let myName = document.getElementById('name');
 myName.onmouseover = () => {
     myName.innerHTML = 'isaac kim';
